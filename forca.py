69b6f993e1bd6535266954e1959f33aff885cf3c
Pedro Henrique <alohapedrohenrique@gmail.com>
2021-04-08 21:20:29 -0400

    Melhorando o jogo da forca

--- forca.py
+++ forca.py
@@ -4,7 +4,39 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    print("Fim do jogo")
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    acertou = False
+    enforcou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Informe uma letra:")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = (erros == 6)
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+
+print("Fim do jogo")
 
 if (__name__ == "__main__"):
     jogar()
